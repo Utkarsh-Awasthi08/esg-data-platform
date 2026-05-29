3c27221dc0f5ffa7503084e77c1923368e0bd413
Utkarsh-Awasthi08 <utkarsha070@gmail.com>
2026-05-29 13:41:22 +0530

    Added health check route

--- esg_backend/backend/urls.py
+++ esg_backend/backend/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 from django.urls import path, include
+def home(request):
 
+    return JsonResponse({
+
+        "status": "ok",
+
+        "message": "ESG Backend Running"
+
+    })
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('ingestion.urls')),
 ]
