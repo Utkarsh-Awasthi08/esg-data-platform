1cef4d763e27bdfe7753dc35e578e849984375ec
Utkarsh-Awasthi08 <utkarsha070@gmail.com>
2026-05-29 13:19:35 +0530

    Frontend deployment setup

--- esg-frontend/src/api/dashboardApi.js
+++ esg-frontend/src/api/dashboardApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:8000/api'
+const BASE_URL = 'https://esg-data-platform-km4q.onrender.com/api'
 
 export const getDashboardData = async () => {
 
